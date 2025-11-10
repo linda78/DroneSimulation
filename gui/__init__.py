a30b3f811a2e6e92cc075bd9631e997b4c3bda1a
Linda Mümken <linda.klesper@gmx.de>
2025-11-10 16:31:42 +0100

    fix for 6486d3167b1f3237ab4cc4c734bf44b092c8dbb0

--- gui/__init__.py
+++ gui/__init__.py
@@ -5,4 +5,25 @@ Uses VisPy for high-performance 3D rendering
 
 from .viewer import DroneViewer
 
+import numpy as np
+
 __all__ = ['DroneViewer']
+
+def lighten_color(color, amount=0.5):
+    """
+    Lightens the given color by multiplying (1-luminosity) by the given amount.
+    Input can be matplotlib color string, hex string, or RGB tuple.
+
+    Examples:
+    >> lighten_color('g', 0.3)
+    >> lighten_color('#F034A3', 0.6)
+    >> lighten_color((.3,.55,.1), 0.5)
+    """
+    import matplotlib.colors as mc
+    import colorsys
+    try:
+        c = mc.cnames[color]
+    except:
+        c = color
+    c = np.array(colorsys.rgb_to_hls(*mc.to_rgb(c)))
+    return colorsys.hls_to_rgb(c[0], 1 - amount * (1 - c[1]), c[2])
